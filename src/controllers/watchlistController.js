e7163c3c605733dd3957342efff7793f5f826b82
Iftekhar Priyo <iftekhar@younode.com>
2026-06-07 17:10:49 +0600

    feat: delete update routes added

--- src/controllers/watchlistController.js
+++ src/controllers/watchlistController.js
@@ -41,4 +41,69 @@ const addToWatchlist = async (req, res) => {
   });
 };
 
+const deleteWatchlistItem = async (req, res) => {
+  const { movieId } = req.params;
+
+  console.log("Deleting movieId:", movieId, "for userId:", req.user.id);
+
+  const watchlistItem = await prisma.watchlistItem.findUnique({
+    where: {
+      userId_movieId: {
+        userId: req.user.id,
+        movieId: movieId,
+      },
+    },
+  });
+
+  if (!watchlistItem) {
+    return res.status(404).json({ error: "Item not found in watchlist" });
+  }
+
+  await prisma.watchlistItem.delete({
+    where: {
+      userId_movieId: {
+        userId: req.user.id,
+        movieId: movieId,
+      },
+    },
+  });
+
+  res.status(200).json({
+    status: "success",
+    message: "Item removed from watchlist",
+  });
+};
+
+const updateWatchlistItem = async (req, res) => {
+  const { movieId } = req.params;
+  const { status, rating, notes } = req.body;
+  const watchlistItem = await prisma.watchlistItem.findUnique({
+    where: {
+      userId_movieId: { userId: req.user.id, movieId: movieId },    
+    },
+  }); 
+    if (!watchlistItem) {
+    return res.status(404).json({ error: "Item not found in watchlist" });
+  }
+  // update
+  // make it optional to update only one field at a time
+  const updatedItem = await prisma.watchlistItem.update({
+    where: {
+      userId_movieId: { userId: req.user.id, movieId: movieId },  
+    },
+    data: {
+      status: status || watchlistItem.status,
+      rating: rating || watchlistItem.rating,
+      notes: notes || watchlistItem.notes,
+    },
+  }); 
+
+  res.status(200).json({
+    status: "success",
+    data: updatedItem,
+  });
+}
+
 exports.addToWatchlist = addToWatchlist;
+exports.deleteWatchlistItem = deleteWatchlistItem;
+exports.updateWatchlistItem = updateWatchlistItem;
--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const { authMiddleware } = require('../middleware/authMiddleware');
-const { addToWatchlist } = require('../controllers/watchlistController');
+const {
+  addToWatchlist,
+  deleteWatchlistItem,
+  updateWatchlistItem
+} = require('../controllers/watchlistController');
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
 router.post('/', addToWatchlist);
+
+router.delete('/delete/:movieId', deleteWatchlistItem);
+
+router.put('/update/:movieId', updateWatchlistItem);
 
 module.exports = router;
